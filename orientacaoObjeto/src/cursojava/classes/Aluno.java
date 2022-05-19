44863ac7aea455319b5b9a144345ab02bb579932
Djailson1 <djailson_silva1@outlook.com>
2022-05-18 23:29:35 -0300

    costrução de um pequeno projeto par fixar conceito de OO

---
+++ orientacaoObjeto/src/cursojava/classes/Aluno.java
@@ -0,0 +1,36 @@
+package cursojava.classes;
+
+public class Aluno {/*esta é a classe que representa aluno*/
+	/*Caracteristicas gerais - atributos e tipos*/
+	
+	/*atributos do aluno*/
+	public String nome;
+	public int idade;
+	public String dataNascimento;
+	String rg;
+	String cpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	/*para ser objeto tem que ser usado em outro local no main por exemplo*/
+
+
+
+	/*parte 2*/
+	/*construtores(comportamentos - métodos - ações )*/
+	
+	public Aluno() {/*construtor padrão java - cria os dados na memória*/
+		/*também se ele não for chamado o java irá escrever por trás porque ele é padrão*/
+	}
+	
+	public Aluno(String nomePadrao) {
+		nome = nomePadrao;
+	}
+	public Aluno(String nomePadrao, int idadePadrao) {
+		nome = nomePadrao;
+		idade = idadePadrao;
+	}
+
+}
---
+++ orientacaoObjeto/src/cursojava/oo/PrimeiraClasse.java
@@ -0,0 +1,34 @@
+package cursojava.oo;
+
+import cursojava.classes.Aluno;
+
+public class PrimeiraClasse {
+	public static void main(String[] args) {
+		/*quando instânciado passa a existir na memória java
+		 */
+		
+		/*new Aluno() é uma instância(criação de Objeto)*/
+		/*aluno1 é uma referência para o objeto aluno*/
+		/*é obrigatório instânciar*/
+		
+		Aluno aluno1 = new Aluno();/*aqui pode ser João*/
+		/*por padrão os atributos de java são privados(private) tem que colocar public lá*/
+	/*classe /variável /ch const. / do tipo de class*/
+		aluno1.nome = "João"; /*referÊncia + atributo passou o nome que foi armazenado*/
+		aluno1.idade = 48;
+		System.out.println("O nome do aluno 1 é = " + aluno1.nome);/*para imprimir*/
+		System.out.println("A idade é = " + aluno1.idade);
+
+		Aluno aluno2 = new Aluno(); /*aqui Maria*/
+		
+		Aluno aluno3 = new Aluno();/*aqui Djailson*/
+		
+		Aluno aluno4 = new Aluno("Bruno");/*parâmetro string foi passado no construtor*/
+		
+		Aluno aluno5 = new Aluno("Jennifer",24);/*2 parâmetros foram passados no contrutor na classe principal main*/
+
+		/*Passou a ser objeto depois que foi instânciado 
+		 (instância é como se fosse o tipo das variáveis no caso o tipo da classe)*/
+	}
+
+}
