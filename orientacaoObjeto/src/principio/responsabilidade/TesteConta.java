d653d2639676f421cf75b261cc544cfb2b7679d3
Djailson1 <djailson_silva1@outlook.com>
2022-05-27 23:52:13 -0300

    aula sobre principio da responsabilidade única - SOLID

---
+++ orientacaoObjeto/src/principio/responsabilidade/TesteConta.java
@@ -0,0 +1,36 @@
+package principio.responsabilidade;
+
+public class TesteConta {
+
+	public static void main(String[] args) {
+		
+		ContaBancaria bancaria = new ContaBancaria();
+		
+		bancaria.setDescricao("Conta bancária do Djailson");
+		
+		System.out.println(bancaria);
+		
+		bancaria.diminue100Reais();
+		bancaria.diminue100Reais();
+		
+		System.out.println(bancaria);
+		
+		bancaria.sacarDinheiro(400);
+		
+		System.out.println(bancaria);
+		
+		bancaria.depositidoDinheiro(1000);
+		
+		System.out.println(bancaria);
+		
+		bancaria.soma100Reais();
+		
+		System.out.println(bancaria);
+		
+		
+		
+		
+
+	}
+
+}
