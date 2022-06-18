a2a471f8d6414a195db232984d0f222f3e8b5712
Djailson1 <djailson_silva1@outlook.com>
2022-06-17 23:44:35 -0300

    adc array de notas dentro da disciplina

--- orientacaoObjeto/src/cursojava/oo/ArrayVetor.java
+++ orientacaoObjeto/src/cursojava/oo/ArrayVetor.java
@@ -2,19 +2,33 @@ package cursojava.oo;
 
 import javax.swing.JOptionPane;
 
+import cursojava.classes.Aluno;
+import cursojava.classes.Disciplina;
+
 public class ArrayVetor {
 	public static void main(String[] args) {
 		
+		double[] notas = {8.9, 6.7, 7.9, 9.0};
+		double[] notasLogica = {9.9, 8.7, 9.9, 6.0};
 		
-		String[] valores = {"Dj", "100", "Curso de Java", "contato@jdev.com"};
+		/*criação do aluno*/
+		Aluno aluno = new Aluno();
+		aluno.setNome("Djailson");
+		aluno.setNomeEscola("Jdev treinamento");
 		
-		for(int pos = 0; pos < valores.length; pos++ ) {
-			
-			System.out.println("Valores na posição " + pos + " é igual " + valores[pos]);
-			
-			
-		}
+		/*criação da disciplina*/
+		Disciplina disciplina = new Disciplina();
+		disciplina.setDisciplina("Curso de Java");
+		disciplina.setNota(notas);
 		
+		
+		aluno.getDisciplinas().add(disciplina);
+		
+		Disciplina disciplina2 = new Disciplina();
+		disciplina2.setDisciplina("Log programação");
+		disciplina2.setNota(notasLogica);
+		
+		aluno.getDisciplinas().add(disciplina2);
 		
 	}
 
