fcbcce83e1a280c853cfebb97dcffde3adc9e9dc
Djailson1 <djailson_silva1@outlook.com>
2022-06-18 20:23:29 -0300

    percorrendo listas e arrays

--- orientacaoObjeto/src/cursojava/oo/ArrayVetor.java
+++ orientacaoObjeto/src/cursojava/oo/ArrayVetor.java
@@ -30,6 +30,21 @@ public class ArrayVetor {
 		
 		aluno.getDisciplinas().add(disciplina2);
 		
+		System.out.println("Nome do aluno = " + aluno.getNome() + " Inscrito no curso : " + aluno.getNomeEscola());
+		System.out.println(" --------- Discilinas do aluno ---------");
+		
+		for (Disciplina d : aluno.getDisciplinas()) {
+			
+			System.out.println("Discilina : " + d.getDisciplina());
+			System.out.println("As notas da disciplina são : " );
+			
+			for(int pos = 0; pos < d.getNota().length; pos++) {
+			
+				System.out.println("NOta " + pos + " é igual = " + d.getNota()[pos]);
+			}
+			
+		}
+		
 	}
 
 }
