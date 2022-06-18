19fffeee741c9d9d66554b6ed1b2d80a4a475f23
Djailson1 <djailson_silva1@outlook.com>
2022-06-17 23:11:07 -0300

    array de string para qualquer tipo de dados

--- orientacaoObjeto/src/cursojava/oo/ArrayVetor.java
+++ orientacaoObjeto/src/cursojava/oo/ArrayVetor.java
@@ -6,29 +6,15 @@ public class ArrayVetor {
 	public static void main(String[] args) {
 		
 		
-		double[] valores = {9.7, 8.7, 7.9, };
+		String[] valores = {"Dj", "100", "Curso de Java", "contato@jdev.com"};
 		
-		/*array pode ser de todos tipos de dados e objetos também*/
-		
-		String posicoes = JOptionPane.showInputDialog("Quantas posições o array deve ter?");
-		
-		
-		/*array sempre deve ter quant. de posições definidas*/
-		double[] notas = new double[Integer.parseInt(posicoes)];
-		
-		for(int pos = 0; pos < notas.length; pos++) {
-			String valor = JOptionPane.showInputDialog("Qual o valor da posição = " + pos);
-			notas[pos] = Double.valueOf(valor);
-					
-		}
-		
-		
-		for(int pos = 0; pos < notas.length; pos++ ) {
+		for(int pos = 0; pos < valores.length; pos++ ) {
 			
-			System.out.println("Nota = " + (pos + 1) + " é = " + notas[pos]);
+			System.out.println("Valores na posição " + pos + " é igual " + valores[pos]);
 			
 			
 		}
+		
 		
 	}
 
