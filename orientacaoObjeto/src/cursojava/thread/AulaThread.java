d5f50785f756f47355d9e96b58cbcf644160786e
Djailson1 <djailson_silva1@outlook.com>
2022-06-21 19:37:03 -0300

    tempo de interrupção com Thread

---
+++ orientacaoObjeto/src/cursojava/thread/AulaThread.java
@@ -0,0 +1,19 @@
+package cursojava.thread;
+
+public class AulaThread {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		for(int pos = 0; pos < 10; pos ++) {
+			
+			/*Quero executar essae envio com tempo de parada ou com tempo determinado */
+			System.out.println("Executando alguma rotina, por exemplo envio de e-mail");
+			
+			Thread.sleep(1000);/*Dá um tempo*/
+			
+		}
+		System.out.println("Chegou ao fim do código de teste de thread");
+		
+	}
+
+}
