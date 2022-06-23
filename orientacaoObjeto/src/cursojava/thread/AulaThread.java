71a09246146823c114d6e3c9208a1af074696f75
Djailson1 <djailson_silva1@outlook.com>
2022-06-22 22:37:01 -0300

    processamento concorrente entre duas Thread

--- orientacaoObjeto/src/cursojava/thread/AulaThread.java
+++ orientacaoObjeto/src/cursojava/thread/AulaThread.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class AulaThread {
 	
 	public static void main(String[] args) throws InterruptedException {
-		/*Thread processando em paralelo*/
+		/*Thread processando em paralelo do envio de e-mail*/
 		new Thread() {
 			public void run() {/*executa o que queremos */
 				/*código da rotina*/
@@ -30,7 +30,33 @@ public class AulaThread {
 			
 		}.start();/*start liga a thread que fica processando paralelamente por trás*/
 		
+		//============================div das thread
 		
+		
+		/*Thread processando em paralelo do envio de nota fiscal*/
+		new Thread() {
+			public void run() {/*executa o que queremos */
+				/*código da rotina*/
+				
+
+				/*código da rotina que quero executar em paralelo*/
+				for(int pos = 0; pos < 10; pos ++) {
+					
+					/*Quero executar essae envio com tempo de parada ou com tempo determinado */
+					System.out.println("Executando alguma rotina, por exemplo envio de nota fiscal");
+					
+					try {
+						Thread.sleep(4000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}/*Dá um tempo*/
+					
+				}
+				/*fim do código em paralelo*/
+			};
+			
+		}.start();/*start liga a thread que fica processando paralelamente por trás*/
 		
 		/*código do sistema do usuário continua o fluxo de trabalho*/
 		System.out.println("Chegou ao fim do código de teste de thread");
