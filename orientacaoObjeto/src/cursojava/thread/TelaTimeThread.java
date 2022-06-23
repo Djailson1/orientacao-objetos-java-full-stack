61130dce4042acb5e6b89269dc7b1e96b368531a
Djailson1 <djailson_silva1@outlook.com>
2022-06-22 23:33:16 -0300

    10 minutos da aula sobre GridBagConstraints ger. de pos e layout

--- orientacaoObjeto/src/cursojava/thread/TelaTimeThread.java
+++ orientacaoObjeto/src/cursojava/thread/TelaTimeThread.java
@@ -1,10 +1,20 @@
 package cursojava.thread;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class TelaTimeThread extends JDialog{
+	
+	private JPanel jPanel = new JPanel(new GridBagLayout());/*Painel de componentes*/
+	
+	private JLabel descricaoHora = new JLabel("Time da Thread 1");
+	
 	
 	public TelaTimeThread() {/*execcuta o que estiver dentro no momento da abertura ou execução */
 		
@@ -14,9 +24,14 @@ public class TelaTimeThread extends JDialog{
 		setResizable(false);/*não deixa redimensionar a tela*/
 		/*primeira parte concluida*/
 		
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();/*controlador de posicionamento de componentes*/
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
 		
+		descricaoHora.setPreferredSize(new Dimension(200, 25));
+		jPanel.add(descricaoHora, gridBagConstraints);
 		
-		
+		add(jPanel, BorderLayout.WEST);
 		/*sempre será o último comando*/
 		setVisible(true);/*torna a tela visível para o usuário*/
 
